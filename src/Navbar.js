88afd5067813705b8b3ba665b7ca235e7dbc8024
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2021-11-21 11:13:34 +0530

    final

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,11 +11,11 @@ const Navbar = ({children}) => {
 	return (
 		<Box sx={{bgcolor:"#0b0a0f",height:"100vh"}} >
 			<Grid conatiner sx={{bgcolor:"orange",width:"100vw",height:"7vh",display:"flex",alignItems:"center"}}>
-				<Grid item xs={4} md={9} sx={{width:"45%",ml:1,display:"flex"}}>
+				<Grid item xs={6} md={9} sx={{width:"45%",ml:1,display:"flex"}}>
 					<Box sx={{mt:"5px",mr:1}}><img src={logo} alt="" width="30px"/></Box>
 					<Typography variant="h4">Aniverse</Typography>
 				</Grid>
-				<Grid item xs={8} md={3} sx={{width:"10%",display:"flex"}} >
+				<Grid item xs={6} md={3} sx={{width:"10%",display:"flex"}} >
 					<Button onClick={()=>change("/")} sx={{color:"white"}} ><Typography variant="h5" sx={{mr:1}}>Home </Typography></Button>
 					<Typography variant="h5" sx={{mt:"3px",color:"wheat"}} > | </Typography>
 					<Button onClick={()=>change("/fun")} sx={{color:"white"}} ><Typography sx={{ml:1}} variant="h5"> Fun</Typography></Button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,16 +25,16 @@ const Home = () => {
 
 	return (
 		<Box sx={{height:"90vh" ,bgcolor:"#0b0a0f",mt:2,overflow:"hidden"}} >
-			<Grid container  sx={{height:"10vh",bgcolor:"transparent",display:"flex",pt:"15px"}} >
-				<Grid item  xs={12}md={2}><Typography variant="h5" sx={{ml:6,color:"wheat",width:"80px",mr:"3vw"}}>find by</Typography></Grid>
-				<Grid item  xs={2}><Chip onClick={()=>click("popular")} label="Popular" color="primary" sx={{mr:"3vw",height:"40px",width:"150px"}} /></Grid>
-				<Grid item  xs={2}><Chip onClick={()=>click("rating")} label="Rating" color="primary" sx={{mx:"3vw",height:"40px",width:"150px"}} /></Grid>
-				<Grid item  xs={2}><Chip onClick={()=>click("new")} label="New" color="primary" sx={{mx:"3vw",height:"40px",width:"150px"}} /></Grid>
-				<Grid item  xs={2}><Chip onClick={()=>click("trending")} label="Trending" color="primary" sx={{mx:"3vw",height:"40px",width:"150px"}} /></Grid>
+			<Grid container  sx={{height:"20vh",bgcolor:"transparent",display:"flex",pt:"15px"}} >
+				<Grid item  xs={12} md={2}><Typography variant="h5" sx={{ml:6,color:"wheat",width:"80px",mr:"3vw"}}>find by</Typography></Grid>
+				<Grid item  xs={6} md={2}><Chip onClick={()=>click("popular")} label="Popular" color="primary" sx={{mr:"3vw",height:"40px",width:"150px"}} /></Grid>
+				<Grid item  xs={6} md={2}><Chip onClick={()=>click("rating")} label="Rating" color="primary" sx={{mx:"3vw",height:"40px",width:"150px"}} /></Grid>
+				<Grid item  xs={6} md={2}><Chip onClick={()=>click("new")} label="New" color="primary" sx={{mx:"3vw",height:"40px",width:"150px"}} /></Grid>
+				<Grid item  xs={6} md={2}><Chip onClick={()=>click("trending")} label="Trending" color="primary" sx={{mx:"3vw",height:"40px",width:"150px"}} /></Grid>
 			</Grid>
 			<Box sx={{mt:5}} >
 
-				<Paper elevation={3} sx={{height:"73vh",borderRadius:2,bgcolor:"#18191a",m:5,mt:4}}>
+				<Paper elevation={3} sx={{height:"60vh",borderRadius:2,bgcolor:"#18191a",m:5,mt:4}}>
 					<Grid container rowSpacing ={5} columnSpacing={1} sx={{height:"100%",width:"100%",ml:3.8,overflowY:"scroll"}}>
 						{list.map(a=> <Grid item xs={12} md={3}>
 						<Card title={a.name} id={a.id} rating={a.score} img={a.img}/>
